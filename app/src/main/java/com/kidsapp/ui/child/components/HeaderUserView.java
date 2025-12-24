0206ad06fb1336092ed20ff09cd91893cf582d35
horapcode <hohuuhuy208@gmail.com>
2025-12-24 18:00:37 +0700

    update home

--- app/src/main/java/com/kidsapp/ui/child/components/HeaderUserView.java
+++ app/src/main/java/com/kidsapp/ui/child/components/HeaderUserView.java
@@ -4,48 +4,85 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.kidsapp.R;
 
-public class HeaderUserView extends ConstraintLayout {
+/**
+ * Header component chung cho các trang Child
+ * Có thể tái sử dụng ở nhiều Fragment khác nhau
+ */
+public class HeaderUserView extends LinearLayout {
     private ImageView imgAvatar;
     private TextView txtUserName;
-    private View btnNotification;
+    private FrameLayout btnNotification;
+    private TextView txtNotificationBadge;
 
-    public HeaderUserView(Context ctx, AttributeSet attrs) {
-        super(ctx, attrs);
-        init(ctx);
+    public HeaderUserView(Context context) {
+        super(context);
+        init(context);
     }
 
-    private void init(Context ctx) {
-        LayoutInflater.from(ctx).inflate(R.layout.view_header_user, this, true);
+    public HeaderUserView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public HeaderUserView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.view_header_user, this, true);
 
         imgAvatar = findViewById(R.id.imgAvatar);
         txtUserName = findViewById(R.id.txtUserName);
         btnNotification = findViewById(R.id.btnNotification);
+        txtNotificationBadge = findViewById(R.id.txtNotificationBadge);
     }
 
+    /**
+     * Set tên người dùng
+     */
     public void setUserName(String name) {
         txtUserName.setText(name);
     }
 
+    /**
+     * Set avatar từ resource
+     */
     public void setAvatar(int imgRes) {
         imgAvatar.setImageResource(imgRes);
     }
 
+    /**
+     * Set click listener cho avatar
+     */
+    public void setAvatarClick(OnClickListener listener) {
+        imgAvatar.setOnClickListener(listener);
+    }
+
+    /**
+     * Set click listener cho nút thông báo
+     */
     public void setNotificationClick(OnClickListener listener) {
         btnNotification.setOnClickListener(listener);
     }
 
     /**
-     * Xử lý sự kiện click vào avatar
-     * @param listener Listener để xử lý khi click vào avatar
+     * Set số lượng thông báo chưa đọc
+     * @param count Số thông báo (0 = ẩn badge)
      */
-    public void setAvatarClick(OnClickListener listener) {
-        imgAvatar.setOnClickListener(listener);
+    public void setNotificationCount(int count) {
+        if (count > 0) {
+            txtNotificationBadge.setVisibility(View.VISIBLE);
+            txtNotificationBadge.setText(count > 9 ? "9+" : String.valueOf(count));
+        } else {
+            txtNotificationBadge.setVisibility(View.GONE);
+        }
     }
 }
